97ebb0f6558d8f657cd500974d32a1353ab9feb6
syukronarie <syukronarie@gmail.com>
2021-09-05 10:54:24 +0700

    add emotionjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,57 @@
 import "./App.css";
 
 import React from "react";
+import { atom, RecoilRoot, selector, useRecoilState, useRecoilValue } from "recoil";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <RecoilRoot>
+      <CharacterCounter />
+    </RecoilRoot>
+  );
+}
+
+function CharacterCounter() {
+  return (
+    <div>
+      <TextInput />
+      <CharacterCount />
+    </div>
+  );
+}
+
+const textState = atom({
+  key: "textState", // unique ID (with respect to other atoms/selectors)
+  default: "" // default value (aka initial value)
+});
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Text: {text}
+    </div>
+  );
+}
+
+const charCountState = selector({
+  key: "charCountState", // unique ID (with respect to other atoms/selectors)
+  get: ({ get }) => {
+    const text = get(textState);
+    return text.length;
+  }
+});
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+  return <>Character Count: {count}</>;
 }
 
 export default App;
